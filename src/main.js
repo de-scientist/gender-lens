7ed25774e75651299272a3afc649bea23a11ac5f
de-scientist <gitaumark502@gmail.com>
2025-10-08 14:28:38 +0300

    add DOM elements

---
+++ src/main.js
@@ -0,0 +1,6 @@
+//add DOM elements
+const input = document.getElementById("form-name-input");
+const btn = document.querySelector(".search-btn");
+const result = document.querySelector(".result");
+
+
