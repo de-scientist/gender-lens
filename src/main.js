500e4ea7299db5fa332c5bf07534f178a042c3ea
de-scientist <gitaumark502@gmail.com>
2025-10-11 08:32:44 +0300

    add an event listener

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ const input = document.getElementById("form-name-input");
 const btn = document.querySelector(".search-btn");
 const result = document.querySelector(".result");
 
+//add an event listener
+btn.addEventListener("click", async (ev) => {
+  ev.preventDefault()
+  const providedName  = input.value;
+
+  if (providedName === "") {
+    result.textContent = `Please provide a name`;
+    return
+  }
+
+})
 
