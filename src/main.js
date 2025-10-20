af11cf997c8c58277b1369d3c909f9ebfdd2326c
de-scientist <gitaumark502@gmail.com>
2025-10-20 22:52:00 +0300

    format the code to make it appealing

--- src/main.js
+++ src/main.js
@@ -5,45 +5,43 @@ const result = document.querySelector(".result");
 
 //add an event listener
 btn.addEventListener("click", async (ev) => {
-  ev.preventDefault()
-  const providedName  = input.value;
+  ev.preventDefault();
+  const providedName = input.value;
 
   //handle empty input
   if (providedName === "") {
     result.textContent = `Please provide a name`;
-    return
+    return;
   }
 
   //fetch data from the api
   try {
-    //disable the button while fetching 
-    btn.disabled= true;
-    btn.textContent = "Searching..."
+    //disable the button while fetching
+    btn.disabled = true;
+    btn.textContent = "Searching...";
 
     //fetch data from the api
-    const response = await fetch(`https://api.genderize.io?name=${providedName}`)
+    const response = await fetch(
+      `https://api.genderize.io?name=${providedName}`,
+    );
     const data = await response.json();
-    const {gender, probability} = data;
+    const { gender, probability } = data;
 
     //handle no data found
     if (data.gender === null) {
       result.textContent = `No data found for the name <strong>${providedName}</strong>`;
       return;
-
     } else {
       //display the result
       result.innerHTML = `<strong>${providedName}</strong> is most likely a <strong>${gender}</strong> with a probability of <strong>${probability * 100}</strong>% certainty.`;
     }
-
   } catch (error) {
     //display error message
-      result.textContent = `An error occurred. Please try again later.`;
-      console.error("Error fetching data:", error);
-
+    result.textContent = `An error occurred. Please try again later.`;
+    console.error("Error fetching data:", error);
   } finally {
-      //enable the button after fetching
-      btn.disabled = false;
-      btn.innerHTML = `<i class="fa-solid fa-venus-mars"></i><span> Search</span>`;
+    //enable the button after fetching
+    btn.disabled = false;
+    btn.innerHTML = `<i class="fa-solid fa-venus-mars"></i><span> Search</span>`;
   }
-})
-
+});
