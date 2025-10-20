6e7e0e4ac654352d43f5539f446e41d5ad3d2b28
de-scientist <gitaumark502@gmail.com>
2025-10-20 22:29:00 +0300

    display the result

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ btn.addEventListener("click", async (ev) => {
     //fetch data from the api
     const response = await fetch(`https://api.genderize.io?name=${providedName}`)
     const data = await response.json();
+    const {gender, probability} = data;
 
     //handle no data found
     if (data.gender === null) {
@@ -33,6 +34,11 @@ btn.addEventListener("click", async (ev) => {
       btn.textContent = "Search";
       return;
 
+    } else {
+      //display the result
+      result.innerHTML = `<strong>${providedName}</strong> is most likely a <strong>${gender}</strong> with a probability of <strong>${probability * 100}</strong>% certainty.`;
+
+      
     }
 
   } catch (error) {
